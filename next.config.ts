eee2c5124a207ef4fbe73ec9d07fc35488beb249
Harsh Carpenter <harshcarpenter06902@gmail.com>
2025-07-23 19:29:49 +0530

    XSS

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,10 @@ const securityHeaders = [
     key: 'Strict-Transport-Security',
     value: 'max-age=63072000; includeSubDomains; preload',
   },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
 ];
 
 const nextConfig: NextConfig = {
@@ -59,7 +63,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)", 
+        source: "/(.*)",
         headers: securityHeaders,
       },
     ];

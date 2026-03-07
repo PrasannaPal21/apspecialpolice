1e5baceecd19701ee23844ca7ebad9470ac09deb
PrasannaPal21 <prasannapal273@gmail.com>
2026-03-07 17:02:58 +0530

    seed

--- seed.js
+++ seed.js
@@ -20,10 +20,17 @@ async function main() {
     await clearUserData();
 
     const usersToCreate = [
-      { name: 'Test User 11', hallticket: 'TEST111', dobPlain: '01-01-1990', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
-      { name: 'Test User 12', hallticket: 'TEST112', dobPlain: '02-02-1991', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
-      { name: 'Test User 13', hallticket: 'TEST113', dobPlain: '03-03-1992', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
-      { name: 'Test User 14', hallticket: 'TEST114', dobPlain: '04-04-1993', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
+      { name: 'Test User 01', hallticket: 'TEST001', dobPlain: '01-01-1990', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
+      { name: 'Test User 02', hallticket: 'TEST002', dobPlain: '02-02-1991', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
+      { name: 'Test User 03', hallticket: 'TEST003', dobPlain: '03-03-1992', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
+      { name: 'Test User 04', hallticket: 'TEST004', dobPlain: '04-04-1993', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
+      { name: 'Test User 05', hallticket: 'TEST005', dobPlain: '05-05-1994', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
+      { name: 'Test User 06', hallticket: 'TEST006', dobPlain: '06-06-1995', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
+      { name: 'Test User 07', hallticket: 'TEST007', dobPlain: '07-07-1996', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
+      { name: 'Test User 08', hallticket: 'TEST008', dobPlain: '08-08-1997', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
+      { name: 'Test User 09', hallticket: 'TEST009', dobPlain: '09-09-1998', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
+      { name: 'Test User 10', hallticket: 'TEST010', dobPlain: '10-10-1999', role: 'USER', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
+
       { name: 'Test Admin', hallticket: 'ADMIN001', dobPlain: '09-09-1985', role: 'ADMIN', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
       { name: 'Test Admin', hallticket: 'ADMIN002', dobPlain: '09-09-1985', role: 'ADMIN', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
       { name: 'Prasanna VIT', hallticket: 'SUPERADMIN001', dobPlain: '01-01-2004', role: 'SUPER_ADMIN', examslot: 'Single Slot', examdate: '2026-03-08', examroom: 'Room A' },
